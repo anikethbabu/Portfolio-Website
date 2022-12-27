e8b85959f370f209f3aa016313a6de4e896c8556
anikethbabu <anikethbabu@gmail.com>
2022-12-27 16:22:03 -0500

    Updated category and ddl scripts. Added dishddl which creates dish table

--- MySqlScripts/ddl.sql
+++ MySqlScripts/ddl.sql
@@ -1,7 +1,7 @@
 CREATE DATABASE RESTAURANT;
 USE RESTAURANT;
 create table IF NOT EXISTS FOOD_CATEGORY(
-CATEGORY_ID int not null auto_increment, 
+CATEGORY_ID int not null, 
 CATEGORY_NAME char(40) NOT NULL, 
 IMAGE_FILE_NAME char(45) NOT NULL,
 primary key (CATEGORY_ID)
